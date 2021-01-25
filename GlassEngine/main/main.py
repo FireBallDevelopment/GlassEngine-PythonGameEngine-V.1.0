48613835e44bd94b213feae7422acf3dff7549bd
FireBallDevelopment <56772897+FireBallDevelopment@users.noreply.github.com>
2021-01-25 18:30:41 +0100

    Intial!

---
+++ GlassEngine/main/main.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+#Version Variable
+version = "V.0.1"
+
+#Window Creation
+root = tk.Tk()
+
+#WindowData
+root.title("Glass Engine " + version)
+root.iconbitmap()
+root.geometry("900x700")
+root.minsize(900, 700)
+
+
+#Window Mainloop()
+root.mainloop()
